fa5e9880c95b40fd4c8823b5b182813e14a98c64
Jacinto Wong <contact@jacinto.design>
2023-10-24 11:30:30 -0400

    final revisions

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
+const goldBar = document.querySelector('.gold-bar');
+const blackBar = document.querySelector('.black-bar');
+const logo = document.querySelector('.img-container');
+const bottomBar = document.querySelector('.bottom-bar');
+const topBar = document.querySelector('.top-bar');
+const textLayer = document.querySelector('.main-bar-text');
+const bottomBarText = document.querySelector('.bottom-bar .bottom-bar-text');
+
 function runAnimationIN() {
-    const goldBar = document.querySelector('.gold-bar');
-    const blackBar = document.querySelector('.black-bar');
-    const logo = document.querySelector('.img-container');
-    const bottomBox = document.querySelector('.bottom-box');
-    const topBox = document.querySelector('.top-box');
-
-    goldBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
-    blackBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
-    logo.style.clipPath = "none";
-
-    logo.style.animation = "revealLeft .5s linear forwards";
-    goldBar.style.animation = "slideIn .5s ease-in-out forwards .1s";
-    blackBar.style.animation = "slideIn .5s ease-in-out forwards .2s";
-    bottomBox.style.animation = "slideDownBottom .6s ease-in-out forwards .2s";
-    topBox.style.animation = "slideUpTop .5s ease-in forwards .2s";
+    // Re-run animations
+    logo.style.animation = 'revealLeft .5s linear forwards';
+    goldBar.style.animation = 'slideIn .5s ease-in-out forwards .1s';
+    blackBar.style.animation = 'slideIn .5s ease-in-out forwards .2s';
+    bottomBar.style.animation = 'slideDownBottom .6s ease-in-out forwards .2s';
+    topBar.style.animation = 'slideUpTop .5s ease-in forwards .2s';
 }
 
 function runAnimationOUT() {
-    const goldBar = document.querySelector('.gold-bar');
-    const blackBar = document.querySelector('.black-bar');
-    const logo = document.querySelector('.img-container');
-    const bottomBox = document.querySelector('.bottom-box');
-    const topBox = document.querySelector('.top-box');
+    // Set clip path to match end of animation in
+    goldBar.style.clipPath = 'polygon(15px 0, 100% 0, 100% 100%, 0% 100%)';
+    blackBar.style.clipPath = 'polygon(15px 0, 100% 0, 100% 100%, 0% 100%)';
+    logo.style.clipPath = 'none';
 
-    goldBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
-    blackBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
-    logo.style.clipPath = "none";
-
-    logo.style.animation = "hideLeft .5s linear forwards";
-    blackBar.style.animation = "slideOut .4s ease-in-out forwards .2s";
-    goldBar.style.animation = "slideOut .4s ease-in-out forwards .3s";
-    bottomBox.style.animation = "slideUpBottom .3s ease-in forwards";
-    topBox.style.animation = "slideDownTop .3s ease-in forwards";
+    // Re-run animations
+    logo.style.animation = 'hideLeft .5s linear forwards';
+    blackBar.style.animation = 'slideOut .4s ease-in-out forwards .2s';
+    goldBar.style.animation = 'slideOut .4s ease-in-out forwards .3s';
+    bottomBar.style.animation = 'slideUpBottom .3s ease-in forwards';
+    topBar.style.animation = 'slideDownTop .3s ease-in forwards';
 }
 
-window.onload = function() {
-    adjustBarWidth();
-    observeTextLayer();
-}
+// On Load: Adjust bar width
+window.onload = () => adjustBarWidth();
 
+// Adjust bar width to fit text
 function adjustBarWidth() {
-    const textLayer = document.querySelector(".main-text");
-    const bottomBoxText = document.querySelector(".bottom-box .bottom-box-text");
-    const goldBar = document.querySelector(".gold-bar");
-    const blackBar = document.querySelector(".black-bar");
-    const bottomBar = document.querySelector(".bottom-box");
-
+    // Get width of main text and bottom text
     const textLayerWidth = textLayer.offsetWidth;
-    const bottomBoxTextWidth = bottomBoxText.offsetWidth;
+    const bottomBarTextWidth = bottomBarText.offsetWidth;
 
+    // Add margin left from main text
     const textLayerStyle = window.getComputedStyle(textLayer);
     const marginLeft = parseFloat(textLayerStyle.getPropertyValue('margin-left'));
 
-    // Use the maximum width of the two texts
-    const barWidth = Math.max(textLayerWidth, bottomBoxTextWidth) + marginLeft;
+    // Use the width of main or bottom text layer, whichever is larger
+    const barWidth = Math.max(textLayerWidth, bottomBarTextWidth) + marginLeft;
 
-    blackBar.style.width = barWidth + "px";
+    // Set widths of bars
+    blackBar.style.width = barWidth + 'px';
     bottomBar.style.width = `${barWidth + 9}px`;
-    goldBar.style.width = (barWidth + 15) + "px";
-}
-
-
-function observeTextLayer() {
-    const textLayer = document.querySelector(".main-text");
-    const bottomBoxText = document.querySelector(".bottom-box .bottom-box-text");
-
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.type === "characterData" || mutation.type === "childList") {
-                adjustBarWidth();
-            }
-        });
-    });
-
-    const config = { childList: true, characterData: true, subtree: true };
-    observer.observe(textLayer, config);
-    observer.observe(bottomBoxText, config);
+    goldBar.style.width = (barWidth + 15) + 'px';
 }
 
 // Keyboard Shortcut to Toggle Animations
 let isGrapicVisible = true;
 
-window.addEventListener('keydown', function(event) {
-        // Check if the pressed key is the spacebar (key code 32)
-        if (event.keyCode === 32) {
-            if (isGrapicVisible) {
-                runAnimationOUT();
-                isGrapicVisible = false;
-            } else {
-                location.reload();
-                isGrapicVisible = true;
-            }
+window.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    // Check if the pressed key code is 'Space' for spacebar
+    if (event.code === 'Space') {
+        if (isGrapicVisible) {
+            runAnimationOUT();
+            isGrapicVisible = false;
+        } else {
+            location.reload();
+            isGrapicVisible = true;
         }
-
-        event.preventDefault();
+    }
 });
